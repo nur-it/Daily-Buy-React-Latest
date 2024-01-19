acece71d7ee329b06223a09b2cb3737b0a966cea
SymuNadeem <msymunnadeem@gmail.com>
2024-01-19 14:57:05 +0600

    issue solve by symun

--- src/components/screen/FeatureProduct.jsx
+++ src/components/screen/FeatureProduct.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import {FiArrowRight} from 'react-icons/fi';
+import { FiArrowRight } from 'react-icons/fi';
 import { Link } from "react-router-dom";
 import ProductCard from "../shared/ProductCard";
 
@@ -35,7 +35,7 @@ const FeatureProduct = () => {
         </Link>
       </div>
       <div className="container">
-        <div className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-[10px] md:gap-[20px]">
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-[10px] md:gap-[20px]">
           {productData.slice(0, 4).map((product) => (
             <ProductCard product={product} key={product._id} />
           ))}
