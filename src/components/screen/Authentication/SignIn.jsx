4a8015796f93166e0436118690791411685e1e55
Al Amin <alaminpgmr@gmail.com>
2024-01-22 11:57:23 +0600

    updated

--- src/components/screen/Authentication/SignIn.jsx
+++ src/components/screen/Authentication/SignIn.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import "../../../styles/SignSignUp.css";
 import { FullWidthButton } from "../../theme/Button";
+
 const SignIn = ({ setPath }) => {
   return (
-    <div className="">
-      <div className="grid grid-cols-1 md:grid-cols-2 items-center h-[75vh] lg:h-full w-full  overflow-y-auto">
-        <div className="w-[400px] lg:w-[430px] h-full hidden md:block bgImg"></div>
-        <div className="p-6 w-full flex justify-center items-center">
-          <div className="w-full">
-            <h2 className="text-2xl font-semibold text-center mb-8 ">Login</h2>
+    <div className="sign-in-container">
+      <div className="grid grid-cols-1 md:grid-cols-2 items-center h-[75vh] lg:h-full w-full overflow-y-auto">
+        <div className="bgImg w-[400px] lg:w-[430px] h-full hidden md:block"></div>
+        <div className="form-container p-6 w-full flex justify-center items-center">
+          <div className="form-content w-full">
+            <h2 className="text-2xl font-semibold text-center mb-8">Login</h2>
             <form onSubmit={(e) => e.preventDefault()} action="#" className="space-y-5">
               <div className="text-center">
                 <input className="px-4 py-3  rounded-lg w-full focus:border-primary-600 outline-none border" type="email" placeholder="Email" />
@@ -19,10 +20,7 @@ const SignIn = ({ setPath }) => {
               </div>
 
               <div className="text-end">
-                <button
-                  onClick={() => setPath("forgetPassword")}
-                  className="inline-block text-secondary-600 font-semibold cursor-pointer hover:underline"
-                >
+                <button onClick={() => setPath("forgetPassword")} className="inline-block text-secondary-600 font-semibold cursor-pointer hover:underline">
                   Forgot password?
                 </button>
               </div>
